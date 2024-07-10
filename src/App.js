95436e3942aad8e1037742e2ec06673b4af6b463
Atul Ahire <atulahire9607@gmail.com>
2024-07-10 14:05:15 +0530

    Creating Signup Screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Layout from './component/Layout/Layout';
+import Authpage from './component/Pages/Authpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>App Start</h1>
+      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Authpage />} />
+        </Routes>
+      </Layout>
+    </>
   );
 }
 
